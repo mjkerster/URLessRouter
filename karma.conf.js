835550f7d3457531bf9aa75d845abdec7f11dc5d
Matt K <mjkerster@gmail.com>
2017-02-28 16:40:41 -0500

    remove unnecessary preprocess

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/*.js': ['webpack', 'coverage'],
+        //'src/*.js': ['webpack', 'coverage'],
         'test/*.test.js': ['webpack']
     },
 
